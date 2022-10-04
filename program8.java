416e25e630b0d56109385c7412ae17763912bc4e
Brian Mak <97199816+y33ted-99@users.noreply.github.com>
2022-10-03 23:38:16 -0700

    Create program8.java

---
+++ program8.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        final I i1 = new A();
+        final I i2 = new B();
+        final AbstractI ai1 = new A();
+        final AbstractI ai2 = new B();
+        final A a = new A();
+        final B b = new B();
+
+        System.out.println("1)");
+        i1.foo();
+        System.out.println("2)");
+        i2.foo();
+        System.out.println("3)");
+        ai1.foo();
+        System.out.println("4)");
+        ai2.foo();
+        System.out.println("5)");
+        a.foo();
+        System.out.println("6)");
+        b.foo();
+
+    }
+}
+
+interface I {
+    void foo();
+
+}
+
+abstract class AbstractI implements I {
+
+}
+
+class A extends AbstractI {
+    @Override
+    public void foo() {
+        System.out.println("A::foo");
+    }
+}
+
+class B extends AbstractI {
+    @Override
+    public void foo() {
+        System.out.println("B::foo");
+    }
+}
